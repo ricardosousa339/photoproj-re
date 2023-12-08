ac83e8cfa7537ab921f1073328069c3754c36364
Ricardo Henrique <ricardosousa339@gmail.com>
2023-12-08 14:53:00 -0300

    Cadastro e Login funcionando

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,7 +3,43 @@ import axios from 'axios';
 import './Login.css';
 import { API_BASE_URL } from '../apiConfig';
 
-const App = () => {
+const LoginForm = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(form);
+      const response = await axios.post(`${API_BASE_URL}`+'api/auth/login/', form);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <label htmlFor="chk" aria-hidden="true">Login</label>
+        <input type="email" name="email" placeholder="Email" required onChange={handleChange} />
+        <input type="password" name="password" placeholder="Senha" required onChange={handleChange} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+const RegisterForm = () => {
   const [form, setForm] = useState({
     username: '',
     password: '',
@@ -18,11 +54,11 @@ const App = () => {
     });
   };
 
-  const handleSubmit = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
     try {
       console.log(form);
-      const response = await axios.post(`${API_BASE_URL}`+'register/', form);
+      const response = await axios.post(`${API_BASE_URL}`+'api/auth/register/', form);
       console.log(response.data);
     } catch (error) {
       console.error(error);
@@ -30,28 +66,25 @@ const App = () => {
   };
 
   return (
+    <div className="signup">
+      <form onSubmit={handleRegister}>
+        <label htmlFor="chk" aria-hidden="true">Cadastro</label>
+        <input type="text" name="username" placeholder="Usuário" required onChange={handleChange} />
+        <input type="email" name="email" placeholder="Email" required onChange={handleChange} />
+        <input type="password" name="password" placeholder="Senha" required onChange={handleChange} />
+        <input type="password" name="password2" placeholder="Confirme a senha" required onChange={handleChange} />
+        <button type="submit">Cadastro</button>
+      </form>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
     <div className="main">
       <input type="checkbox" id="chk" aria-hidden="true" />
-
-      <div className="signup">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="chk" aria-hidden="true">Cadastro</label>
-          <input type="text" name="username" placeholder="Usuário" required onChange={handleChange} />
-          <input type="email" name="email" placeholder="Email" required onChange={handleChange} />
-          <input type="password" name="password" placeholder="Senha" required onChange={handleChange} />
-          <input type="password" name="password2" placeholder="Confirme a senha" required onChange={handleChange} />
-          <button type="submit">Cadastro</button>
-        </form>
-      </div>
-
-      <div className="login">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="chk" aria-hidden="true">Login</label>
-          <input type="text" name="username" placeholder="Usuário" required onChange={handleChange} />
-          <input type="password" name="password" placeholder="Senha" required onChange={handleChange} />
-          <button type="submit">Login</button>
-        </form>
-      </div>
+      <RegisterForm />
+      <LoginForm />
     </div>
   );
 };
